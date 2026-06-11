16c15e109101d02c1fb59eef8beb63f77a59e589
olivier <texmex430@gmail.com>
2026-06-11 15:10:17 +0200

    FastAPI – Create, Update and Delete with Database

--- mainv2.py
+++ mainv2.py
@@ -57,23 +57,31 @@ def get_product_by_id(id: int, db: Session = Depends(get_db)):
     return "product not found"
 
 @app.post("/product")
-def add_product(product:Product):
-    products.append(product)
+def add_product(product:Product, db: Session = Depends(get_db)):
+    db.add(database_models.Product(**product.model_dump()))
+    db.commit()
     return product
 
 
 @app.put("/product")
-def update_product(id: int, product: Product):
-    for i in range(len(products)):
-        if products[i].id == id:
-            products[i] = product
-            return "Product Added successfully"
-    return "No product found"
+def update_product(id: int, product: Product, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db_product.name = product.name
+        db_product.description = product.description
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+        db.commit()
+        return "Product updated"
+    else:
+        return "No product found"
 
 @app.delete("/product")
-def delete_product(id: int):
-    for i in range(len(products)):
-        if products[i].id == id:
-            del products[i]
-            return "Product Deleted"
+def delete_product(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Product).filter(database_models.Product.id == id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+        return "Product Deleted"
+    else:
         return "Product not found"
